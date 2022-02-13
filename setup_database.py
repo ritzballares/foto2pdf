7d696cd697076e5d393bf646d53d0627de964303
Ritz Ballares <ritzballares@gmail.com>
2022-02-13 17:40:53 -0500

    Added database setup script

---
+++ setup_database.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+import os
+import mysql.connector
+
+
+try:
+    load_dotenv()
+
+    print('Connecting to server...')
+    connection = mysql.connector.connect(
+        host = os.environ.get('host'),
+        user = os.environ.get('user'),
+        password = os.environ.get('password')
+    )
+
+    cursor = connection.cursor()
+
+    print('Creating database...')
+    cursor.execute("CREATE DATABASE foto2pdf")
+
+    print('Creating table...')
+    cursor.execute("USE foto2pdf")
+    cursor.execute("CREATE TABLE users(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, images_dir VARCHAR(30), pdf_dir VARCHAR(30), created TIMESTAMP DEFAULT CURRENT_TIMESTAMP, modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)")
+
+    connection.commit()
+    connection.close()
+
+    print('Database setup successful.')
+except mysql.connector.Error as err:
+    print(f'An error occurred while setting up database: {err}')
