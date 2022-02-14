878a3106572ef3f12f10b4adad652610a0585908
Ritz Ballares <ritzballares@gmail.com>
2022-02-14 01:16:14 -0500

    Updated helpers and cleaned up some duplications in app.py; removed model.py

--- app.py
+++ app.py
@@ -1,35 +1,11 @@
 from flask import Flask, request, jsonify, send_from_directory
-from helpers import check_files
-from pathlib import Path
-from werkzeug.utils import secure_filename
-from flask_mysqldb import MySQL
-from dotenv import load_dotenv
-from fpdf import FPDF
-from PIL import Image
-from glob import glob
 from markupsafe import escape
-from model import FotoToPdf
-import os
 import mysql.connector
+import helpers
 
-
-SUPPORTED_EXTENSIONS = {
-    'jpg': True,
-    'jpeg': True,
-    'png': True
-}
-UPLOAD_FOLDER = 'static/client'
 
 app = Flask(__name__)
-load_dotenv()
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MYSQL_HOST'] = os.environ.get('host')
-app.config['MYSQL_USER'] = os.environ.get('user')
-app.config['MYSQL_PASSWORD'] = os.environ.get('password')
-app.config['MYSQL_DB'] = os.environ.get('db')
-
-mysql = MySQL(app)
+app.config['UPLOAD_FOLDER'] = 'static/client'
 
 @app.route('/convert', methods=['POST'])
 def convert():
@@ -39,95 +15,96 @@ def convert():
 
     files = request.files.getlist('images')
 
-    images_are_valid = check_files(files)
+    images_are_valid = helpers.check_files(files)
     if not images_are_valid:
         msg = {'error': 'unsupported file type found'}
         return jsonify(msg), 400
 
-    pdf = FotoToPdf()
-    converted_successfully = pdf.convert(files, app.config['UPLOAD_FOLDER'])
-
-    if not converted_successfully:
+    try:
+        connection = helpers.connect_to_database()
+    except mysql.connector.Error:
         return '', 500
 
-    msg = {'success': 'true', 'id': f'{pdf.get_id()}'}
+    cursor = connection.cursor()
+
+    try:
+        id = helpers.get_new_id(cursor)
+    except mysql.connector.Error:
+        return '', 500
+
+    images_dir = helpers.get_images_dir(id, app.config['UPLOAD_FOLDER'])
+    pdf_dir = helpers.get_pdf_dir(id, app.config['UPLOAD_FOLDER'])
+
+    helpers.create_directories(images_dir, pdf_dir)
+
+    try:
+        helpers.insert_directories_into_database(cursor, images_dir, pdf_dir)
+    except mysql.connector.Error:
+        return '', 500
+
+    helpers.save_images(files, images_dir)
+    helpers.convert_images_to_pdf(images_dir, pdf_dir)
+
+    connection.commit()
+    connection.close()
+
+    msg = {'success': 'true', 'id': f'{id}'}
     return jsonify(msg), 200
 
 @app.route('/convert/<id>', methods=['PUT'])
 def update(id):
+    if "images" not in request.files:
+        msg = {'error': 'images part not found'}
+        return jsonify(msg), 400
+
+    # Connect to database
+    try:
+        connection = helpers.connect_to_database()
+    except mysql.connector.Error:
+        return '', 500
+
+    cursor = connection.cursor()
+
+    # Check if id exists
     id = escape(id)
 
-    cursor = mysql.connection.cursor()
-    id_exists = cursor.execute(f"SELECT * FROM users WHERE id='{id}'")
+    try:
+        id_exists = helpers.check_id_exists(cursor, id)
+    except mysql.connector.Error:
+        return '', 500
 
     if not id_exists:
         msg = {'error': 'id does not exist'}
         return jsonify(msg), 400
 
-    if "images" not in request.files:
-        msg = {'error': 'images part not found'}
-        return jsonify(msg), 400
-
+    # Get uploaded files
     files = request.files.getlist('images')
 
-    # Check if file type is supported
-    for file in files:
-        filename_split = file.filename.split(".")
-        extension = filename_split[-1]
+    # Check if the uploaded files are valid
+    images_are_valid = helpers.check_files(files)
+    if not images_are_valid:
+        msg = {'error': 'unsupported file type found'}
+        return jsonify(msg), 400
 
-        if extension not in SUPPORTED_EXTENSIONS:
-            msg = {'error': f'unsupported file type - {extension}'}
-            return jsonify(msg), 400
+    # Get path for images and pdf
+    try:
+        images_dir, pdf_dir = helpers.get_directories(cursor, id)
+    except mysql.connector.Error:
+        return '', 500
 
-    cursor.execute(f"SELECT images_dir, pdf_dir FROM users WHERE id='{id}'")
-    directories = cursor.fetchall()
+    helpers.remove_existing_files(images_dir, pdf_dir)
+    helpers.save_images(files, images_dir)
+    helpers.convert_images_to_pdf(images_dir, pdf_dir)
 
-    # Indexing twice gets the directories by itself (no parenthesis and commas)
-    images_dir = directories[0][0]
-    pdf_dir = directories[0][1]
+    # Update timestamp in database
+    try:
+        helpers.update_timestamp(cursor, id)
+    except mysql.connector.Error:
+        return '', 500
 
-    # Remove files inside images_dir
-    images_files_dir = glob(os.path.join(images_dir, '*'))
+    connection.commit()
+    connection.close()
 
-    for image_file in images_files_dir:
-        os.remove(image_file)
-
-    # Remove pdf file inside pdf_dir
-    pdf_file_dir = glob(os.path.join(pdf_dir, '*'))
-
-    for pdf_file in pdf_file_dir:
-        os.remove(pdf_file)
-
-    for file in files:
-        # Save uploaded images
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(images_dir, filename))
-    
-    # Create PDF
-    image_filenames = []
-
-    for extension in SUPPORTED_EXTENSIONS:
-        image_filenames.extend(glob(f'{images_dir}/*{extension}'))
-
-    pdf = FPDF()
-    pdf.oversized_images = 'DOWNSCALE'
-
-    for image in image_filenames:
-        img = Image.open(f'{image}')
-        img_width, img_height = img.size
-
-        # Calculate new height to so the image does not go out of bounds
-        # Divide the image height by its width and multiply the result with the pdf's max width
-        new_height = (img_height / img_width) * pdf.epw
-
-        pdf.add_page()
-        pdf.image(img, 0, 0, pdf.epw, new_height)
-
-    pdf.output(f'{pdf_dir}/foto2pdf.pdf')
-
-    cursor.execute(f"UPDATE users SET modified=now() WHERE id='{id}'")
-    mysql.connection.commit()
-    cursor.close()
     msg = {'success': 'true', 'id': f'{id}'}
     return jsonify(msg), 200
 
@@ -135,14 +112,27 @@ def update(id):
 def download(id):
     id = escape(id)
 
-    cursor = mysql.connection.cursor()
-    cursor.execute(f"SELECT pdf_dir FROM users WHERE id='{id}'")
-    pdf_dir = cursor.fetchall()
-    cursor.close()
+    try:
+        connection = helpers.connect_to_database()
+    except mysql.connector.Error:
+        return '', 500
+
+    cursor = connection.cursor()
+
+    try:
+        pdf_dir = helpers.get_pdf_dir_from_id(cursor, id)
+    except mysql.connector.Error:
+        return '', 500
+
+    connection.close()
 
     if len(pdf_dir) == 0:
         msg = {'error': 'id does not exist'}
         return jsonify(msg), 400
 
-    pdf_dir = pdf_dir[0][0] # Gets the pdf_directory by itself (no parenthesis and commas)
+    # Get the pdf_directory by itself (no parenthesis and commas) by indexing twice
+    pdf_dir = pdf_dir[0][0] 
     return send_from_directory(pdf_dir, 'foto2pdf.pdf')
+
+if __name__ == '__main__':
+    app.run()
--- helpers.py
+++ helpers.py
@@ -1,8 +1,19 @@
+from dotenv import load_dotenv
+from pathlib import Path
+from werkzeug.utils import secure_filename
+from fpdf import FPDF
+from glob import glob
+from PIL import Image
+import mysql.connector
+import os
+
+
 SUPPORTED_EXTENSIONS = {
     'jpg': True,
     'jpeg': True,
     'png': True
 }
+
 
 def check_files(files):
     """Checks if the uploaded files are valid.
@@ -21,3 +32,137 @@ def check_files(files):
             return False
 
     return True
+
+def connect_to_database():
+    try:
+        load_dotenv()
+
+        connection = mysql.connector.connect(
+            host = os.environ.get('host'),
+            user = os.environ.get('user'),
+            password = os.environ.get('password'),
+            database = os.environ.get('db')
+        )
+    except mysql.connector.Error as err:
+        print('Error:', err)
+        raise
+
+    return connection
+
+def get_new_id(cursor):
+    try:
+        cursor.execute('SELECT * FROM users')
+        cursor.fetchall()
+    except mysql.connector.Error as err:
+        print('Error:', err)
+        raise
+
+    num_rows = cursor.rowcount
+
+    user_id = num_rows + 1
+    user_id = str(user_id)
+
+    return user_id
+
+def get_images_dir(id, upload_folder):
+    return os.path.join(upload_folder, id, 'images')
+
+def get_pdf_dir(id, upload_folder):
+    return os.path.join(upload_folder, id, 'pdf')
+
+def create_directories(images_dir, pdf_dir):
+    #Path(os.path.join(self.upload_folder, self.id)).mkdir(parents=True, exist_ok=True)
+    Path(images_dir).mkdir(parents=True, exist_ok=True)
+    Path(pdf_dir).mkdir(parents=True, exist_ok=True)
+
+def insert_directories_into_database(cursor, images_dir, pdf_dir):
+    cursor.execute(f"INSERT INTO users(images_dir, pdf_dir) VALUES('{images_dir}', '{pdf_dir}')")
+
+def save_images(files, images_dir):
+    for file in files:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(images_dir, filename))
+
+def convert_images_to_pdf(images_dir, pdf_dir):
+    image_filenames = []
+
+    for extension in SUPPORTED_EXTENSIONS:
+        image_filenames.extend(glob(f'{images_dir}/*{extension}'))
+
+    pdf = FPDF()
+    pdf.oversized_images = 'DOWNSCALE'
+
+    for image in image_filenames:
+        img = Image.open(f'{image}')
+        img_width, img_height = img.size
+
+        # Calculate new height to so the image does not go out of bounds
+        # Divide the image height by its width and multiply the result with the pdf's max width
+        new_height = (img_height / img_width) * pdf.epw
+
+        pdf.add_page()
+        pdf.image(img, 0, 0, pdf.epw, new_height)
+
+    pdf.output(f'{pdf_dir}/foto2pdf.pdf')
+
+def get_pdf_dir_from_id(cursor, id):
+    try:
+        cursor.execute(f"SELECT pdf_dir FROM users WHERE id='{id}'")
+    except mysql.connector.Error as err:
+        print('Error:', err)
+        raise
+
+    pdf_dir = cursor.fetchall()
+    return pdf_dir
+
+def check_id_exists(cursor, id):
+    try:
+        cursor.execute(f"SELECT * FROM users WHERE id='{id}'")
+    except mysql.connector.Error as err:
+        print('Error:', err)
+        raise
+
+    row = cursor.fetchall()
+
+    if len(row) == 0:
+        return False
+
+    return True
+
+def get_directories(cursor, id):
+    try:
+        cursor.execute(f"SELECT images_dir, pdf_dir FROM users WHERE id='{id}'")
+    except mysql.connector.Error as err:
+        print('Error:', err)
+        raise
+
+    directories = cursor.fetchall()
+
+    # Indexing twice gets the directories by itself (no parenthesis and commas)
+    images_dir = directories[0][0]
+    pdf_dir = directories[0][1]
+
+    return images_dir, pdf_dir
+
+def remove_existing_files(images_dir, pdf_dir):
+    remove_existing_images(images_dir)
+    remove_existing_pdf(pdf_dir)
+
+def remove_existing_images(images_dir):
+    images_files_dir = glob(os.path.join(images_dir, '*'))
+
+    for image_file in images_files_dir:
+        os.remove(image_file)
+
+def remove_existing_pdf(pdf_dir):
+    pdf_file_dir = glob(os.path.join(pdf_dir, '*'))
+
+    for pdf_file in pdf_file_dir:
+        os.remove(pdf_file)
+
+def update_timestamp(cursor, id):
+    try:
+        cursor.execute(f"UPDATE users SET modified=now() WHERE id='{id}'")
+    except mysql.connector.Error as err:
+        print('Error:', err)
+        raise
