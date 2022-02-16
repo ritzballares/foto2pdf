0aaaf20372ca9dd92129d67d17a4bb7c6f1bffea
Ritz Ballares <ritzballares@gmail.com>
2022-02-16 16:33:40 -0500

    Added docstrings for db.py and foto2pdf.py; Slightly modified convert_images_to_pdf in foto2pdf.py; Changed images_dir and pdf_dir to images_path and pdf_path in database setup script

--- db.py
+++ db.py
@@ -1,9 +1,11 @@
+from dotenv import load_dotenv
 import os
 import mysql.connector
-from dotenv import load_dotenv
+
 
 class Db:
     def __init__(self):
+        """Initializes a Db object."""
         load_dotenv()
         self.connection = mysql.connector.connect(
             host = os.environ.get('host'),
@@ -14,25 +16,29 @@ class Db:
         self.cursor = self.connection.cursor()
 
     def get_row_count(self):
+        """Gets the number of rows in the users table."""
         self.cursor.execute('SELECT * FROM users')
         self.cursor.fetchall()
 
         num_rows = self.cursor.rowcount
         return num_rows
 
-    def insert_record(self, images_dir, pdf_dir):
-        self.cursor.execute(f"INSERT INTO users(images_dir, pdf_dir) VALUES('{images_dir}', '{pdf_dir}')")
+    def insert_record(self, images_path, pdf_path):
+        """Inserts a record into the users table."""
+        self.cursor.execute(f"INSERT INTO users(images_path, pdf_path) VALUES('{images_path}', '{pdf_path}')")
 
-    def get_pdf_dir_from_id(self, id):
-        self.cursor.execute(f"SELECT pdf_dir FROM users WHERE id='{id}'")
+    def get_pdf_path_from_id(self, id):
+        """Gets the path to the PDF directory with the associated id."""
+        self.cursor.execute(f"SELECT pdf_path FROM users WHERE id='{id}'")
         
-        pdf_dir = self.cursor.fetchall()
-        if len(pdf_dir) == 0:
+        pdf_path = self.cursor.fetchall()
+        if len(pdf_path) == 0:
             return ''
         else:
-            return pdf_dir[0][0]
+            return pdf_path[0][0]
 
     def id_exists(self, id):
+        """Checks if the id exists in the users table."""
         self.cursor.execute(f"SELECT * FROM users WHERE id='{id}'")
         record = self.cursor.fetchall()
 
@@ -42,5 +48,6 @@ class Db:
             return True
 
     def close(self):
+        """Commits the changes made to the database and closes the connection."""
         self.connection.commit()
         self.connection.close()
--- foto2pdf.py
+++ foto2pdf.py
@@ -5,28 +5,47 @@ from fpdf import FPDF
 from PIL import Image
 import os
 
+
 class FotoToPdf:
     def __init__(self, images_path, pdf_path):
+        """Initializes a FotoToPdf object.
+
+        Parameters:
+            images_path: a path to the images directory.
+            pdf_path: a path to the pdf directory.
+        """
         self.images_path = images_path
         self.pdf_path = pdf_path
 
     def convert(self, images):
+        """ Converts the images passed to this function into a pdf.
+
+        Parameters:
+            images: a list of images uploaded by the user.
+        """
         self.save_images(images)
         self.convert_images_to_pdf()
 
     def save_images(self, images):
+        """Saves the images uploaded by the user into the images directory.
+
+        Parameters:
+            images: a list of images uploaded by the user.        
+        """
         for image in images:
             image_filename = secure_filename(image.filename)
             image.save(os.path.join(self.images_path, image_filename))
 
     def convert_images_to_pdf(self):
+        """Converts the images in the images directory into a PDF and stores it into the PDF directory."""
         image_filenames = []
 
-        for extension in SUPPORTED_EXTENSIONS:
-            image_filenames.extend(glob(f'{self.images_path}/*{extension}'))
+        pdf = FPDF()
+        pdf.oversized_images = 'DOWNSCALE'
 
-            pdf = FPDF()
-            pdf.oversized_images = 'DOWNSCALE'
+        for extension in SUPPORTED_EXTENSIONS:
+            # Get all filenames with the extension
+            image_filenames.extend(glob(f'{self.images_path}/*{extension}'))
 
             for image in image_filenames:
                 img = Image.open(f'{image}')
@@ -39,4 +58,6 @@ class FotoToPdf:
                 pdf.add_page()
                 pdf.image(img, 0, 0, pdf.epw, new_height)
 
-            pdf.output(f'{self.pdf_path}/foto2pdf.pdf')
+            image_filenames.clear()
+
+        pdf.output(f'{self.pdf_path}/foto2pdf.pdf')
